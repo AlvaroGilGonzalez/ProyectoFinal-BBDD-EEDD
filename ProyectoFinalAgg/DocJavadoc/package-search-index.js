2c6f8599e2d0872a7b5a4b71498cd2209e2f7613
Álvaro Gil González <alvaro220999@gmail.com>
2022-05-28 11:57:42 +0200

    Añadimos la documentacion generada con Javadoc y todos los diagramas.

---
+++ ProyectoFinalAgg/DocJavadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"GestionBD"},{"l":"Main"},{"l":"Test"}];updateSearchResults();
---
+++ ProyectoFinalAgg/DocJavadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"GestionBD","l":"DBManager"},{"p":"Main","l":"GestionClientes"}];updateSearchResults();
